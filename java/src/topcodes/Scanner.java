778558c6ce8c55a871f2b281d166334580cf87d5
mew <mew>
2014-10-04 16:41:09 -0400

    Added support to scan OpenCV Mat objects for topcodes.

--- java/src/topcodes/Scanner.java
+++ java/src/topcodes/Scanner.java
@@ -27,11 +27,14 @@ package topcodes;
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
+import java.awt.image.DataBufferByte;
 import java.awt.image.BufferedImage;
-
 import java.io.File;
 import java.io.IOException;
+
 import javax.imageio.ImageIO;
+
+import org.opencv.core.Mat;
 
 import java.util.List;
 
